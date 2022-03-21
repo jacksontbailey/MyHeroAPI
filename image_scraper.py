8cc9decb1c75f2e74bc2938a587de92b4d5a4a65
Jackson Bailey <bailey95jackson@gmail.com>
2022-03-21 16:08:23 -0400

    Added in regex code for separating card names from their url and then downloading each image from their url and saving it as the card name.

--- image_scraper.py
+++ image_scraper.py
@@ -1,4 +1,4 @@
-import requests, re
+import requests, re, asyncio, shutil
 from cgitb import text
 from pprint import pprint
 from bs4 import BeautifulSoup
@@ -9,13 +9,32 @@ page = requests.get(parent_url).text
 soup = BeautifulSoup(page, "html.parser")
 imgs = soup.find_all('img')
 
+async def download_card_images():
+    for card in cards.values():
+        pattern = re.compile(r'\d+(-([a-z])+)+(-\d).png$', re.IGNORECASE)
+        file_name_search = pattern.search(card)
+        file_name= file_name_search.group()
+        res = requests.get(card, stream = True)
+        if res.status_code == 200:
+            with open(file_name, 'wb') as f:
+                shutil.copyfileobj(res.raw, f)
+            print(f'Image successfully Downloaded: {file_name}')
+        else:
+            print('Image couldn\'t be retrieved')
+            
 cards={}
 i = 0
+
 for img in (imgs):
     if img.has_attr(r'srcset'):
         card = (img['src'])
         cards[i] =card
         i+=1
-pprint(cards)
 
-print(cards[3])
+download_card_images()
+
+"""card_name = (cards[1])
+print(card_name)
+pattern = re.compile(r'\d+(-([a-z])+)+(-\d).png$', re.IGNORECASE)
+mo = pattern.search(card_name)
+print(f"Here is the pattern I found: {mo.group()}")"""
