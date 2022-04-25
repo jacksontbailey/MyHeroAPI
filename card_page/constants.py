4e657851e5dcbdf8b3f9a10b407f153246bd77b0
Jackson Bailey <bailey95jackson@gmail.com>
2022-04-25 16:10:31 -0400

    Parsing the data based on the card type

--- card_page/constants.py
+++ card_page/constants.py
@@ -1,4 +1,7 @@
 BASE_URL = "https://sitemap.tcgplayer.com/universus.0.xml"
+TEST_URL = "https://www.tcgplayer.com/product/251196/universus-universus-my-hero-academia-capture-evil-doers"
+
+# -- Regex Expressions
 BASE_URL_REGEX = r"(product\/)\d+\/(universus-universus-my-hero-academia)((?!((\-.+)+)(booster-((box)|(pack))|pack)))((\-.+)+)((?<!\-xr)$)((?<!\-xsr)$)"
 PARSE_JSON_REGEX = r"((https:\/\/)((.+)+)(?=<\/))"
-TEST_URL = "https://www.tcgplayer.com/product/251196/universus-universus-my-hero-academia-capture-evil-doers"
+CARD_NUMBER = r"(\/\d+)"
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-import requests, re, json, asyncio
+import re
+
+from sqlalchemy import desc
 import card_page.constants as const
 from card_page.card_page import Card_Page
 from requests.exceptions import HTTPError
@@ -14,9 +16,40 @@ def run_test():
     # -- Remove if/else statement to cycle through all of them. 
         loop = 0
         for i, val in enumerate(get_all_urls):
-            if loop < 10:
+            if loop < 20:
                 description = bot.open_all_card_urls(get_all_urls[i])
                 print(val, description)
+
+    #-- Find out if the card is a Character, Attack, Foundation, Action, or Asset
+
+                card_name = description[0]
+                card_description_unparsed = description[1]
+                card_rarity = description[2][0][1]
+                card_number = description[2][1][1]
+                card_type = description[2][2][1]
+                card_resource_symbols_unparsed = description[2][3][1]
+                card_check = description[2][4][1]
+                card_difficulty = description[2][5][1]
+                card_block_modifier = description[2][6][1]
+                card_block_zone = description[2][7][1]
+
+                if card_type == "Character":
+                    None
+                elif card_type == "Attack":
+                    None
+                elif card_type == "Foundation":
+                    None
+                elif card_type == "Action":
+                    None
+                elif card_type== "Asset":
+                    None
+                else:
+                    None
+
+                card_number = re.sub(const.CARD_NUMBER, '', card_number)
+                
+                print(card_number)
+                print(card_type)
                 loop+=1
                 print(f"{loop} loop total")
             else:
