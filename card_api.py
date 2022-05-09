2bee73ee7891913329a2b1a9de80947d74a9e3fb
Jackson Bailey <bailey95jackson@gmail.com>
2022-05-09 16:16:16 -0400

    Working on getting Unicode_Parser to erase all of the unicode in the card keywords

--- card_api.py
+++ card_api.py
@@ -6,170 +6,203 @@ app = FastAPI()
 
 with open("./MHAcards.json") as f:
     card_database = json.load(f)
+    u = Unicode_Parser()
+
+    full_card_results = []
 
     for val, card in enumerate(card_database):
         c = card_database[val]
-        card_type = card.get("card_type")
-        if card_type == "Attack":
-            new_card = AttackCard(name=c["card_name"], 
+        ct = card.get("card_type")
+        kw = card.get("card_keywords")
+        revised_keywords = u.parse_list(kw)
+
+        #print(ct)
+
+        # -- Parses cards by their types since each type has a different amount of 
+        # -- attributes that it paseses in.  
+        if ct == "Attack":
+            new_card = Card(name=c["card_name"], 
                             id=c["card_number"],
+                            type_attributes= {"type": ct,
+                                "attack_zone": c['card_attack_zone'],
+                                "speed": c["card_attack_speed"],
+                                "attack_keywords": c["card_keywords"],
+                                "damage": c["card_attack_damage"]
+                            },
+                            card_type=c["card_type"],
                             rarity=c["card_rarity"],
                             play_difficulty=c['card_difficulty'],
                             block_modifier= c['card_block_modifier'],
                             block_zone=c['card_block_zone'],
-                            text_box=c["card_description"],
+                            description=c["card_description"],
                             symbols=c["card_resource_symbols"],
                             check=c["card_check"],
-                            keyword=c["card_keywords"],
-                            speed=c["card_attack_speed"],
-                            damage=c["card_attack_damage"]
                             )
-            print(new_card)
-        elif card_type == "Asset":
-            new_card = AssetCard(name=c["card_name"], 
+            full_card_results.append(new_card)
+            #print(full_card_results)
+
+        elif ct == "Asset":
+            new_card = Card(name=c["card_name"], 
                             id=c["card_number"],
+                            type_attributes={"type": ct},
                             rarity=c["card_rarity"],
+                            card_type=c["card_type"],
                             play_difficulty=c['card_difficulty'],
                             block_modifier= c['card_block_modifier'],
                             block_zone=c['card_block_zone'],
-                            text_box=c["card_description"],
+                            description=c["card_description"],
                             symbols=c["card_resource_symbols"],
                             check=c["card_check"],
                             keyword=c["card_keywords"]
                             )
-            print(new_card)
-        elif card_type == "Action":
-            new_card = ActionCard(name=c["card_name"], 
+            full_card_results.append(new_card)
+            #print(full_card_results)
+
+
+        elif ct == "Action":
+            new_card = Card(name=c["card_name"], 
                             id=c["card_number"],
+                            type_attributes= {"type": ct},
                             rarity=c["card_rarity"],
                             play_difficulty=c['card_difficulty'],
                             block_modifier= c['card_block_modifier'],
                             block_zone=c['card_block_zone'],
-                            text_box=c["card_description"],
-                            symbols=c["card_resource_symbols"],
+                            description=c["card_description"],
+                            symbols= c["card_resource_symbols"],
                             check=c["card_check"],
                             keyword=c["card_keywords"]
                             )
-            print(new_card)
-        elif card_type == "Character":
-            new_card = CharacterCard(name=c["card_name"], 
+            full_card_results.append(new_card)
+            #print(full_card_results)
+
+        elif ct == "Character":
+            new_card = Card(name=c["card_name"], 
                             id=c["card_number"],
+                            type_attributes= {
+                                "type": ct,
+                                "starting_hand_size": c["card_hand_size"],
+                                "max_health": c["card_vitality"]
+},
                             rarity=c["card_rarity"],
                             play_difficulty=c['card_difficulty'],
                             block_modifier= c['card_block_modifier'],
                             block_zone=c['card_block_zone'],
-                            text_box=c["card_description"],
+                            description=c["card_description"],
                             symbols=c["card_resource_symbols"],
                             check=c["card_check"],
                             keyword=c["card_keywords"],
-                            starting_hand_size=c["card_hand_size"],
-                            max_health=c["card_vitality"]
                             )
-            print(new_card)
-        elif card_type == "Foundation":
-            new_card = FoundationCard(name=c["card_name"], 
+            full_card_results.append(new_card)
+            #print(full_card_results)
+
+        elif ct == "Foundation":
+            new_card = Card(name=c["card_name"], 
                             id=c["card_number"],
+                            type_attributes= {"type": ct},
                             rarity=c["card_rarity"],
                             play_difficulty=c['card_difficulty'],
                             block_modifier= c['card_block_modifier'],
                             block_zone=c['card_block_zone'],
-                            text_box=c["card_description"],
+                            description=c["card_description"],
                             symbols=c["card_resource_symbols"],
                             check=c["card_check"],
                             keyword=c["card_keywords"],
                             )
-            print(new_card)
+            full_card_results.append(new_card)
+            #print(full_card_results)
+
         else:
             print("Card not in types")
 
+print(full_card_results)
     
 
 
-#@app.get("/")
-#async def home():
-#    return {"Greeting": "Welcome to the My Hero Academia Card Game API! This is a fan-made API that any developer is free to use. The only thing I ask is that you give me some credit when you use it, and/or buy me a coffee. This carbon-based lifeform needs Java installed..."}
-#
-#@app.get("/card-list")
-#async def card_list():
-#    return {"cards": []}
-#
-#@app.get("/get-card/{card_id}")
-#def get_card(card_id: int = Path(None, description="The ID of the item you'd like to add.")):
-#    return card_database[card_id]
-#
-#@app.get("/get-card-by-name")
-#def get_card(name: str = Query(None, title = "Name", description="Name of item.")):
-#    for card_id in card_database:
-#        if card_database[card_id].name == name:
-#            return card_database[card_id]
-#    return {"Data": "Not found"}
-#
-#@app.get("/get-card-by-name/{card_id}")
-#def get_card( *, card_id: int, name: Optional[str] = None, test: int):
-#    for card_id in card_database:
-#        if card_database[card_id].name == name:
-#            return card_database[card_id]
-#    return {"Data": "Not found"}
-#
-#
-#@app.post("/add-card/{card_id}")
-#def add_card(card_id: int, card: cards.Card):
-#    if card_id in card_database:
-#        return{"Error": "Card already exists in database."}
-#    
-#    card_database[card_id] = card
-#    return card_database[card_id]
-#
-#@app.put("/update-card/{card_id}")
-#def update_card(card_id: int, card: cards.UpdateCard):
-#    if card_id not in card_database:
-#        return{"Error": "Card does not exist in database."}
-#    
-#    if card.id != None:
-#        card_database[card_id].id = card.id
-#    
-#    if card.image_url != None:
-#        card_database[card_id].image_url = card.image_url
-#    
-#    if card.name != None:
-#        card_database[card_id].name = card.name
-#    
-#    if card.type != None:
-#        card_database[card_id].type = card.type
-#    
-#    if card.rarity != None:
-#        card_database[card_id].rarity = card.rarity
-#    
-#    if card.play_difficulty != None:
-#        card_database[card_id].play_difficulty = card.play_difficulty
-#
-#    if card.block_modifier != None:
-#        card_database[card_id].block_modifier = card.block_modifier
-#    
-#    if card.block_zone != None:
-#        card_database[card_id].block_zone = card.block_zone
-#    
-#    if card.text_box != None:
-#        card_database[card_id].text_box = card.text_box
-#    
-#    if card.symbols != None:
-#        card_database[card_id].symbols = card.symbols
-#
-#    if card.check != None:
-#        card_database[card_id].check = card.check
-#
-#    if card.keyword != None:
-#        card_database[card_id].keyword = card.keyword
-#
-#    return card_database[card_id]
-#
-#@app.delete("/delete-card")
-#def delete_card(card_id: int = Query(..., description= "The ID of the card to delete must be greater than or equal to 0.", gt=0)):
-#    if card_id not in card_database:
-#        return {"Error": "ID does not exist."}
-#    
-#    del card_database[card_id]
-#    return{"Success": "Item deleted"}
+@app.get("/")
+async def home():
+    return {"Greeting": "Welcome to the My Hero Academia Card Game API! This is a fan-made API that any developer is free to use. The only thing I ask is that you give me some credit when you use it, and/or buy me a coffee. This carbon-based lifeform needs Java installed..."}
+
+@app.get("/card-list")
+async def card_list():
+    return {"cards": full_card_results}
+
+@app.get("/get-card/{card_id}")
+def get_card(card_id: int = Path(None, description="The ID of the item you'd like to add.")):
+    return full_card_results[card_id]
+
+@app.get("/get-card-by-name")
+def get_card(name: str = Query(None, title = "Name", description="Name of item.")):
+    for card_id in full_card_results:
+        if full_card_results[card_id].name == name:
+            return full_card_results[card_id]
+    return {"Data": "Not found"}
+
+@app.get("/get-card-by-name/{card_id}")
+def get_card( *, card_id: int, name: Optional[str] = None, test: int):
+    for card_id in full_card_results:
+        if full_card_results[card_id].name == name:
+            return full_card_results[card_id]
+    return {"Data": "Not found"}
+
+
+@app.post("/add-card/{card_id}")
+def add_card(card_id: int, card: cards.Card):
+    if card_id in full_card_results:
+        return{"Error": "Card already exists in database."}
+    
+    full_card_results[card_id] = card
+    return full_card_results[card_id]
+
+@app.put("/update-card/{card_id}")
+def update_card(card_id: int, card: cards.UpdateCard):
+    if card_id not in full_card_results:
+        return{"Error": "Card does not exist in database."}
+    
+    if card.id != None:
+        full_card_results[card_id].id = card.id
+    
+    if card.image_url != None:
+        full_card_results[card_id].image_url = card.image_url
+    
+    if card.name != None:
+        full_card_results[card_id].name = card.name
+    
+    if card.type != None:
+        full_card_results[card_id].type = card.type
+    
+    if card.rarity != None:
+        full_card_results[card_id].rarity = card.rarity
+    
+    if card.play_difficulty != None:
+        full_card_results[card_id].play_difficulty = card.play_difficulty
+
+    if card.block_modifier != None:
+        full_card_results[card_id].block_modifier = card.block_modifier
+    
+    if card.block_zone != None:
+        full_card_results[card_id].block_zone = card.block_zone
+    
+    if card.description != None:
+        full_card_results[card_id].description = card.description
+    
+    if card.symbols != None:
+        full_card_results[card_id].symbols = card.symbols
+
+    if card.check != None:
+        full_card_results[card_id].check = card.check
+
+    if card.keyword != None:
+        full_card_results[card_id].keyword = card.keyword
+
+    return full_card_results[card_id]
+
+@app.delete("/delete-card")
+def delete_card(card_id: int = Query(..., description= "The ID of the card to delete must be greater than or equal to 0.", gt=0)):
+    if card_id not in card_database:
+        return {"Error": "ID does not exist."}
+    
+    del card_database[card_id]
+    return{"Success": "Item deleted"}
 # / root with api explaination
 # /card-names
 # /cards-by-index/{index}
--- cards.py
+++ cards.py
@@ -1,9 +1,16 @@
 import re
+
+from numpy import append
 import card_page.constants as const
 from enum import Enum
-from typing import Optional, Literal, Union
-from pydantic import BaseModel, ValidationError, validator
+from typing import Annotated, Dict, Optional, Literal, Union
+from pydantic import BaseModel, Field, ValidationError, validator
 
+
+class Zone(str, Enum):
+    high = "High"
+    mid = "Mid"
+    low = "Low"
 class Symbol(str, Enum):
     air = "Air"
     all = "All"
@@ -36,111 +43,91 @@ class AttackKeyword(str, Enum):
     weapon = "Weapon"
     unique = "Unique"
 
+class CharacterCard(BaseModel):
+    type: Literal['Character']
+    starting_hand_size: int
+    max_health: int
+
+class AttackCard(BaseModel):
+    type: Literal['Attack']
+    attack_zone: Zone
+    speed: int
+    damage: int
+    attack_keywords: list[AttackKeyword]
+    ability: Optional[str]
+
+class FoundationCard(BaseModel):
+    type: Literal['Foundation']
+class ActionCard(BaseModel):
+    type: Literal['Action']
+class AssetCard(BaseModel):
+    type: Literal['Asset']
+
 class Card(BaseModel):
     name: str
     id: int
+    type_attributes: Annotated[Union[CharacterCard, AttackCard, AssetCard, ActionCard,FoundationCard], Field(discriminator='type')]
     rarity: str
     image_url: str | None = None
     play_difficulty: int
     block_modifier: int
     block_zone: str
-    text_box: list[str]
+    description: list[str]
     symbols: list[Symbol]
     check: int
     keyword: Optional[list] | None = None
 
 class UpdateCard(BaseModel):
-    id: Optional[int] = None
-    image_url: Optional[str]= None
     name: Optional[str] = None
+    id: Optional[int] = None
+    type_attributes: Optional[str] = None
     rarity: Optional[str] = None
+    image_url: Optional[str]= None
     play_difficulty: Optional[int] = None
     block_modifier: Optional[int] = None
     block_zone: Optional[str] = None
-    text_box: Optional[list[str]] = None
+    description: Optional[list[str]] = None
     symbols: Optional[list[Symbol]] = None
     check: Optional[int] = None
     keyword: Optional[list[str]] = None
 
-class CharacterCard(Card):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
-
-    type = "Character"
-    starting_hand_size: int
-    max_health: int
-
-class AttackCard(Card):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
-
-    type = "Attack"
-    zone = "High" or "Mid" or "Low"
-    speed: int
-    damage: int
-    keyword: list[AttackKeyword]
-    ability: Optional[str]
-
-class FoundationCard(Card):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
-    
-    type = "Foundation"
-class ActionCard(Card):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
-    
-    type = "Action"
-class AssetCard(Card):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
-    
-    type = "Asset"
 
 class Unicode_Parser():
-
     def parse_list(self, new_data):
         for number, string in enumerate(new_data):
-#            if (new_data[number] == "Keywords") | (new_data[number] == ""):
-#                print(f"{string} is a keyword")
-
             unicode = re.search(const.UNICODE_SEARCH, string)
+            unicode_swap = None
+            print(new_data[number])
+            if new_data[number] == "Keywords":
+                new_data.pop(number)
+            if new_data[number] == "":
+                new_data.pop(number)
 
-            if unicode == r"\u2019":
+            if unicode == r"\\u2019":
                 unicode_swap = re.sub(unicode, "\'", string)
-                return(unicode_swap)
+                append(unicode_swap)         
 
-            elif unicode == r"\u2022":
+            if unicode == r"\\u2022":
                 unicode_swap = re.sub(unicode, "", string)                
-                return(unicode_swap)
+                append(unicode_swap)         
 
-            elif unicode == r"\u201c":
+            if unicode == r"\\u201c":
                 unicode_swap = re.sub(unicode, "\"", string)
-                return(unicode_swap)
+                append(unicode_swap)         
             
-            elif unicode == r"\u201d":
+            if unicode == r"\\u201d":
                 unicode_swap = re.sub(unicode, "\"", string)
-                return(unicode_swap)
+                append(unicode_swap)         
 
-            elif unicode == r"\u2022":
+            if unicode == r"\\u2022":
                 unicode_swap = re.sub(unicode, "", string)
-                return(unicode_swap)
+                append(unicode_swap)         
+            
+            if unicode == True:
+                unicode_swap = re.sub(unicode, "", string)
+                append(unicode_swap)         
             else:
                 print(f"No unicode in string: {string}")    
 
-                
-    
-class Height(str, Enum):
-    tall = "tall"
-    medium = "medium"
-    short = "short"
-class Parent_Class(BaseModel):
-    eyes: str
-    height: Height
-    weight: int
-
-class Child_Class(Parent_Class):
-    def __init_subclass__(cls) -> None:
-        return super().__init_subclass__()
-    name: str = "Peter"
-    
+            print(unicode_swap)
+            return unicode_swap
