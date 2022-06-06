49bd6b79956527e2976cb07411a07fa8e2aec583
Jackson Bailey <bailey95jackson@gmail.com>
2022-06-06 15:53:13 -0400

    Finished creating basic authentication with cryptography

--- API_Base/security_classes.py
+++ API_Base/security_classes.py
@@ -9,3 +9,10 @@ class User(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
--- API_Base/security_paths.py
+++ API_Base/security_paths.py
@@ -1,6 +1,8 @@
-from api_base.security_classes import User, UserInDB
-from api_base.security_funct import fake_hash_password, fake_users_db, get_current_active_user
-from fastapi import Depends, HTTPException, Form, APIRouter
+from datetime import timedelta
+from .security_classes import Token, User
+from .security_consts import ACCESS_TOKEN_EXPIRE_MINUTES
+from .security_funct import authenticate_user, create_access_token, fake_users_db, get_current_active_user
+from fastapi import Depends, HTTPException, Form, APIRouter, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 
@@ -13,23 +15,26 @@ router = APIRouter()
 async def login(username: str = Form(), password: str = Form()):
     return {"username":  username}
 
-@router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
+@router.post("/token", response_model = Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail= "Incorrect username or password",
+            headers= {"WWW-Authenticate": "Bearer"}
+        )
+    access_token_expires = timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data = {"sub": user.username}, expires_delta= access_token_expires
+    )
     
-    if not user_dict:
-        raise HTTPException(status_code = 400, detail = "Username doesn't exist", headers="Username doesn't exist")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code = 400, detail = "Incorrect username or password", headers="Incorrect username or password")
-    
-    return {"access_token": user.username, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 
-@router.get("/users/me")
+@router.get("/users/me/", response_model = User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
