dd33b7f5cc78ff0a0d243d04047d71d7dd76b151
Jackson Bailey <bailey95jackson@gmail.com>
2022-11-16 16:22:16 -0500

    Added getServerSideProps to search page to render items. Working on having json parse to html

--- frontend/pages/v1/cards/search/index.js
+++ frontend/pages/v1/cards/search/index.js
@@ -3,6 +3,12 @@ import { useRouter } from 'next/router'
 import {useState, useEffect} from 'react'
 
 
+export async function getServerSideProps(context){
+    return {
+        props: {}
+    }
+}
+
 const CardSearch = () => {
     const router = useRouter()
     const [cards, setCards] = useState()
@@ -28,11 +34,89 @@ const CardSearch = () => {
         }
         getCard();
     }, [])
-    
     return (
         <>
             <h1>Cards</h1>
-            {JSON.stringify(cards)}
+            {(cards === undefined)? <p>No items</p> :
+                Object.values(cards).map((b) => {
+                    if(Array.isArray(b)){
+                        console.log(b)
+                        {Object.values(b).map((c) => {
+                            console.log(c)
+                            return(
+                                <>
+                                    <h2 key={c}>{c}</h2>
+                                    {Object.entries(c).map((d, e) => {
+                                        if(Array.isArray(e)){
+                                            console.log(e)
+                                            return(
+                                                <>
+                                                    <h3 key={d}>{d}</h3>
+                                                    <div key={e}>
+                                                        <h4>{e}</h4>
+                                                        <ul>{e.map(item =>{
+                                                            console.log(item)
+                                                            return <li key={item}>{item}</li>
+                                                        })}</ul>
+                                                    </div>
+                                                </>
+                                            )
+                                        } else if (Object.prototype.toString.call(e) === '[object Object]'){
+                                            Object.entries(e).map((ea, eb) => {
+                                                if(Array.isArray(eb)){
+                                                    console.log(eb)
+                                                    return(
+                                                        <>
+                                                            <h5 key={ea}>{ea}</h5>
+                                                            <div key={eb}>
+                                                                <h6>{eb}</h6>
+                                                                <ul>{eb.map(item =>{
+                                                                    console.log(item)
+                                                                    return <li key={item}>{item}</li>
+                                                                })}</ul>
+                                                            </div>
+                                                        </>
+                                                    )
+                                                } else if (Object.prototype.toString.call(eb) === '[object Null]'){
+                                                    return(
+                                                        <>  
+                                                            <h5>{ea}</h5>
+                                                            <p>Null</p>
+                                                        </>
+                                                    )
+                                                } else {
+                                                    return(
+                                                        <>
+                                                            <h5>{ea}</h5>
+                                                            <p>{eb}</p>
+                                                        </>
+                                                    )
+                                                }
+                                            })
+                                        } else if (Object.prototype.toString.call(e) === '[object Null]'){
+                                            return(
+                                                <>  
+                                                    <h3>{d}</h3>
+                                                    <p>Null</p>
+                                                </>
+                                            )
+                                        } else {
+                                            return(
+                                                <>
+                                                    <h3>{d}</h3>
+                                                    <p>{e}</p>
+                                                </>
+                                            )
+                                        }
+        
+                                        }
+                                    )}
+                                </>
+                            )
+                        })}
+                    }
+                })
+            }
         </>
     );
 }
